2b48f9062ba05b233e1dcd64fbfff92858e6925b
Naraina D <narainad308@gmail.com>
2022-02-24 03:56:26 +0530

    css

--- src/mycomponents/TodoItems.js
+++ src/mycomponents/TodoItems.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-export const TodoItems = ({todo, onDelete}) => {
+export const TodoItems = ({ todo, onDelete }) => {
+  let todostyle = {
+    background: "#212529",
+    padding: "10px",
+    color: "white",
+    borderRadius: "10px"
+   }
+
   return (
     <>
-    <div>
+    <div className='todo' style={todostyle}>
       <h4>{todo.title }</h4>
       <p>{todo.des}</p>
       <button className="btn btn-danger btn-sm" onClick={() => { onDelete(todo) }} >Delete</button>
